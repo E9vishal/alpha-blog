57da14c8844fd911fecd3897ea85f0b12d774153
Vishal Biradar <vishalbiradar205@gmail.com>
2018-06-25 16:22:16 +0530

    set secure password for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,19 @@
 class User 
   include Mongoid::Document
   include Mongoid::Timestamps
-  has_many :articles, dependent: :destroy
+  include ActiveModel::SecurePassword
 
+  has_many :articles, dependent: :destroy
+   	
   VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, 
                                     length: {maximum: 105},
-                                    format: {with: VALID_EMAIL_REGEX }
+                                    format: { with: VALID_EMAIL_REGEX }
  
   field :username, type: String
   field :email, type: String
+  field :password_digest, type: String
+  has_secure_password
+
 end
