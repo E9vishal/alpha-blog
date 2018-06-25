5d7b82bc6508284108b2b18456fb8d113a52fa2b
Vishal Biradar <vishalbiradar205@gmail.com>
2018-06-25 13:29:14 +0530

    added the association relations to user and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,12 @@
 class Article
-   
-
-  
   include Mongoid::Document
   include Mongoid::Timestamps
+  belongs_to :user, foreign_key: :user_id, :class_name => 'User'
   field :title, type: String
   field :description, type: String
   
   validates :title, presence: true
   validates :description, presence: true
+  validates :user_id, presence: true
  end
 
